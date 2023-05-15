689edb1b1e76de669c11e7b80b111c22e3354bef
havehope <1976836@gmail.com>
2023-05-15 15:11:14 +0700

    update

---
+++ baitap.good-fast-cheap/GoodFastCheap.js
@@ -0,0 +1,19 @@
+function GFC(gfc){
+
+let x=document.getElementById('g').value;
+let y=document.getElementById('f').value;
+let z=document.getElementById('c').value;
+if(gfc=='g1'){
+    x+=1;
+}
+if(gfc=='f1'){
+    y+=1;
+}
+if(gfc=='c1'){
+    z+=1;
+}
+
+if(x%2==0 && y%2==0 && z%2==0){
+
+}
+}
--- baitap.tinhcannangcothe/tinhCanNang.js
+++ baitap.tinhcannangcothe/tinhCanNang.js
@@ -1,10 +1,17 @@
+
+function tinhToan() {
 let chieucao = document.getElementById("cm").value;
 let cannang = document.getElementById("kg").value;
 let bmi = cannang / (chieucao * chieucao);
-if(bmi < 0.185) {
-    document.writeln("Underweight");
-} else if(0.185 <= bmi && bmi < 0.25) {
-    document.writeln("Normal");
-} else if(0.25 <= bmi && bmi < 0.3) {
-    document.writeln("Overweight");
-} else document.writeln("Obese");
+let ketqua="";
+    if (bmi < 0.00185) {
+        ketqua ="Underweight";
+    } else if (0.00185 <= bmi && bmi < 0.0025) {
+        ketqua ="Normal";
+    } else if (0.0025 <= bmi && bmi < 0.003) {
+        ketqua ="Overweight";
+    } else ketqua ="Obese";
+
+    // document.writeln(bmi);
+    document.getElementById("kq").innerHTML = ketqua;
+}
